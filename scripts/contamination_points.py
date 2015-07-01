72f34928a9eb779017fb765d284e547130916682
Tiffany Chu <tiffanyformosa@gmail.com>
2015-07-01 16:59:45 -0700

    updated contamination_points methods

--- scripts/contamination_points.py
+++ scripts/contamination_points.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
+import struct
+from numpy import arange
+from math import cos, sin, acos, sqrt
+
 import rospy
 import tf
 import std_msgs.msg
 import geometry_msgs.msg
-from numpy import arange
-from math import cos, sin, acos, sqrt
-from sensor_msgs.msg import PointCloud
+from sensor_msgs.msg import PointCloud, ChannelFloat32
 from visualization_msgs.msg import Marker
 
 class Contamination:
 	def __init__(self):
+		self.lgreen = struct.unpack('f', struct.pack('i', 0x00ff00))[0]
+		#self.dgreen = struct.unpack('f', struct.pack('i', 0x007755))[0]
 		self.step = 0.2
 		self.is_contaminated=False
 		#Point32 list to hold points that mark contam
 		self.contaminated = []
+		self.color = ChannelFloat32(name="rgb")
 		self.listener=''
 		self.publisher=''
 
@@ -23,9 +28,10 @@ class Contamination:
 		for x in arange(lower_left[0], upper_right[0], self.step):
 			for y in arange(lower_left[1], upper_right[1], self.step):
 				self.contaminated.append(geometry_msgs.msg.Point32(x, y, 0))
+				self.color.values.append(self.lgreen)
 
 	#turn ellipse into points
-	def get_ellipse_points(self,ellipse):
+	def get_ellipse_data(self,ellipse):
 		#transform map to laser to use this
 		try:
 			(trans,rot) = self.listener.lookupTransform('/map', '/laser', rospy.Time(0))
@@ -33,35 +39,32 @@ class Contamination:
 			a = ellipse.scale.x
 			b = ellipse.scale.y
 			theta = acos(ellipse.pose.orientation.w)*2
-			ellipse_points=[]
-			#outline square that fits ellipse and then find points within ellipse
-			for x in arange(round(center[0]-a/2, 2), round(center[0]+a/2, 2), self.step):
-				for y in arange(round(center[1]-a/2, 2), round(center[1]+a/2, 2), self.step):
-					distance = (pow(((cos(theta)*(x-center[0])+sin(theta)*(y-center[1]))/a), 2) +
-								pow(((sin(theta)*(x-center[0])+cos(theta)*(y-center[1]))/b), 2))
-					if distance < 1:
-						ellipse_points.append((x, y))
-			return ellipse_points
+			return center, a, b, theta
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			pass
 
 	def check_contam(self, ellipse):
 		#if person isn't yet contaminated check to see if they have become contaminated
-		ellipse_points=self.get_ellipse_points(ellipse) #convert marker to points
+		(center, a, b, theta)=self.get_ellipse_data(ellipse) #convert marker to points
 		if not self.is_contaminated:
 			#check if person overlaps with contaminated stuff
-			for point in ellipse_points:
-				for c in self.contaminated:
-					if abs(point[0]-c.x)<0.1 and abs(point[1]-c.y)<0.1:
-						self.is_contaminated=True
-						break
-				if self.is_contaminated==True:
+			for c in self.contaminated:
+				distance = (pow(((cos(theta)*(c.x-center[0])+sin(theta)*(c.y-center[1]))/a), 2) +
+							pow(((sin(theta)*(c.x-center[0])+cos(theta)*(c.y-center[1]))/b), 2))
+			# if person is in area flip is_contaminated to true and break
+				if distance < 1:
+					self.is_contaminated = True
 					break
-			# if person is in area flip is_contaminated to true
-		#if person is contaminated after that check, append all points to the contaminated list
+		#if person is contaminated after that check, append points to the contaminated list
 		if self.is_contaminated:
-			for point in ellipse_points:
-				self.contaminated.append(geometry_msgs.msg.Point32(point[0], point[1], 0))
+			#outline square that fits ellipse and then find points within ellipse
+			for x in arange(round(center[0]-a, 2), round(center[0]+a, 2), self.step):
+				for y in arange(round(center[1]-a, 2), round(center[1]+a, 2), self.step):
+					distance = (pow(((cos(theta)*(x-center[0])+sin(theta)*(y-center[1]))/a), 2) +
+								pow(((sin(theta)*(x-center[0])+cos(theta)*(y-center[1]))/b), 2))
+					if distance < 1: #within ellipse
+						self.contaminated.append(geometry_msgs.msg.Point32(x, y, 0))
+						self.color.values.append(self.lgreen)
 
 	#initialize node
 	def contamination_node(self):
@@ -72,7 +75,8 @@ class Contamination:
 		self.listener = tf.TransformListener()
 		rate = rospy.Rate(10.0)
 		while True:
-			cloud=PointCloud(header=std_msgs.msg.Header(stamp=rospy.Time.now(),frame_id = "map"), points=self.contaminated)
+			cloud=PointCloud(header=std_msgs.msg.Header(stamp=rospy.Time.now(),frame_id = "map"),
+							 points=self.contaminated, channels=[self.color])
 			self.publisher.publish(cloud)
 			if rospy.is_shutdown():
 				break
